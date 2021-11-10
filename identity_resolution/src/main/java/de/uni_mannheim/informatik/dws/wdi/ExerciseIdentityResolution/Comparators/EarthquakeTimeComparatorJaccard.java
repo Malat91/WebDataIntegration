73578e0da7c82e16a03e268bbae16a319607bddb
Mala <malat91@gmail.com>
2021-11-10 13:07:12 +0100

    Added time comparators

---
+++ identity_resolution/src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Comparators/EarthquakeTimeComparatorJaccard.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2017 Data and Web Science Group, University of Mannheim, Germany (http://dws.informatik.uni-mannheim.de/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators;
+
+import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.Comparator;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.ComparatorLogger;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.TokenizingJaccardSimilarity;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Earthquake;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Movie;
+
+/**
+ * {@link Comparator} for {@link Movie}s based on the {@link Movie#getTitle()}
+ * value and their {@link TokenizingJaccardSimilarity} value.
+ * 
+ * @author Robert Meusel (robert@dwslab.de)
+ * @author Oliver Lehmberg (oli@dwslab.de)
+ * 
+ */
+public class EarthquakeTimeComparatorJaccard implements Comparator<Earthquake, Attribute> {
+
+	private static final long serialVersionUID = 1L;
+	private TokenizingJaccardSimilarity sim = new TokenizingJaccardSimilarity();
+	
+	private ComparatorLogger comparisonLog;
+
+	@Override
+	public double compare(
+			Earthquake record1,
+			Earthquake record2,
+			Correspondence<Attribute, Matchable> schemaCorrespondences) {
+		
+		LocalTime localTime = LocalTime.now();
+		
+		//String s1 = record1.getTime().toString();
+		//String s2 = record2.getTime().toString();
+		String s1= DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM).format(record1.getTime());
+		String s2= DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM).format(record2.getTime());
+    	
+    	double similarity = sim.calculate(s1, s2);
+    	
+		if(this.comparisonLog != null){
+			this.comparisonLog.setComparatorName(getClass().getName());
+		
+			this.comparisonLog.setRecord1Value(s1);
+			this.comparisonLog.setRecord2Value(s2);
+    	
+			this.comparisonLog.setSimilarity(Double.toString(similarity));
+		}
+		
+		return similarity;
+	}
+
+	@Override
+	public ComparatorLogger getComparisonLog() {
+		return this.comparisonLog;
+	}
+
+	@Override
+	public void setComparisonLog(ComparatorLogger comparatorLog) {
+		this.comparisonLog = comparatorLog;
+	}
+
+}
